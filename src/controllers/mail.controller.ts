bbd0c7e19d185c7346c77f25e737e67492386611
Angus109 <tobechinlems01@gmail.com>
2024-10-02 21:46:39 -0700

    upadted

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -26,4 +26,56 @@ export const sendMail =async  (receiver_email:any, subject:any, email_body:any)=
        console.log(ex);
        
     }
+
 }
+
+export const testSM =async () =>{
+    try{
+       sendMail("angusdev.aeworks@gmail.com", 'EasyTopUp', "welcome to Easytopup")
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+
+
+
+// import { CONFIG } from '../config';
+// const config = CONFIG();
+
+// export const sendMail = async (receiver_email: any, subject: any, email_body: any) => {
+//   try {
+//     // Replace with your SMTP server settings (e.g., Gmail, Outlook)
+//     const smtpHost = 'smtp.your_smtp_server.com';
+//     const smtpPort = 587; // Or use the appropriate port for your server
+//     const smtpUser = 'your_email@your_domain.com';
+//     const smtpPassword = 'your_email_password';
+
+//     // Create a new transport object using Nodemailer
+//     const transporter = nodemailer.createTransport({
+//       host: smtpHost,
+//       port: smtpPort,
+//       secure: false, // Or true if using SSL
+//       auth: {
+//         user: smtpUser,
+//         pass: smtpPassword,
+//       },
+//     });
+
+//     // Compose the email message
+//     const mailOptions = {
+//       from: `${process.env.SEND_GRID_NAME} <${process.env.SEND_GRID_EMAIL}>`,
+//       to: receiver_email,
+//       subject,
+//       text: 'Email from Easytopup',
+//       html: email_body,
+//     };
+
+//     // Send the email
+//     const info = await transporter.sendMail(mailOptions);
+//     console.log('Message sent: %s', info.messageId);
+//   } catch (error) {
+//     console.error('Error sending email:', error);
+//   }
+// };
--- src/routes/affiliate.route.ts
+++ src/routes/affiliate.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router()
 import auth from '../middlewares/auth'
+import { testSM } from "../controllers/mail.controller";
 
 import { createAffiliate, AuthAfiliate, getAffiliate, getAllAffiliates, deleteAffililates, UpdateAffiliate, verifyAccount } from "../controllers/affiliate.controller";
 
@@ -12,6 +13,8 @@ router.get('/',auth, getAffiliate)
 router.post('/auth', AuthAfiliate)
 router.post('/verify_account', verifyAccount )
 router.put('/', auth, UpdateAffiliate)
+router.post('/testSM', testSM)
+
 
 
 export default router;
