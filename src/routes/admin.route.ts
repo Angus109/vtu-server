463da4fd2dca009ea1ce77f9523df8cfb5dc9635
Angus109 <tobechinlems01@gmail.com>
2024-10-10 07:55:48 -0700

    updated

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -2,9 +2,12 @@ import express from 'express'
 const router = express.Router()
 import auth from '../middlewares/auth'
 import { createAdmin, AuthAdmin, getAdmin } from '../controllers/admin.controller'
+import { suspendAffililates } from '../controllers/affiliate.controller'
 
  router.post('/', createAdmin )
  router.post('/auth', AuthAdmin)
  router.get('/', auth, getAdmin)
+ router.put('/', suspendAffililates)
+ 
 
  export default router
