ac8758a141ba1f2ab663377cac6f220c7b317d3f
Angus109 <tobechinlems01@gmail.com>
2024-10-11 00:32:10 -0700

    updated

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -7,7 +7,7 @@ import { suspendAffililates } from '../controllers/affiliate.controller'
  router.post('/', createAdmin )
  router.post('/auth', AuthAdmin)
  router.get('/', auth, getAdmin)
- router.put('/', suspendAffililates)
+ router.put('/action', auth, suspendAffililates)
  
 
  export default router
